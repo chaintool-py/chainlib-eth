e73924622c9bc405129f558546ca2dc5010d5393
nolash <dev@holbrook.no>
2021-09-13 16:42:00 +0200

    Set default pythonpath

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -a
 set -e
 set -x
-#export PYTHONPATH=${PYTHONPATH:.}
+default_pythonpath=$PYTHONPATH:.
+export PYTHONPATH=${default_pythonpath:-.}
+>&2 echo using pythonpath $PYTHONPATH
 for f in `ls tests/*.py`; do
 	python $f
 done
 set +x
 set +e
+set +a
